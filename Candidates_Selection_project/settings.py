052319caa3e916ac696ba7f895d612ff884f36e6
cph0r <68689440+chiragphor4@users.noreply.github.com>
2020-09-08 11:31:35 +0000

    gc commit #1

--- Candidates_Selection_project/settings.py
+++ Candidates_Selection_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '+qryf_#ix&(-s=k4%qu&u2yy^ii(-c7wiwj@=$m_8w9vf8#olf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = [ '*','32.126.117.215',
+                  ]
 
 
 # Application definition
