8a6603463e7ebd622863071831e015aa96235932
Sadaf Shaikh <65351945+sadaf-shaikh@users.noreply.github.com>
2020-11-05 19:47:08 +0530

    sort_by_date_and_modified_tables

--- static/csp_app/js/tables.js
+++ static/csp_app/js/tables.js
@@ -14,9 +14,9 @@
             "lengthMenu": [10, 25, 50, 75, 100],
             
         });
-        $('#vendor-table').DataTable({
-            "order": [[ 2, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":2, "type":"date-eu"}],
+        $('#vendor-table').DataTable({          
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
@@ -24,8 +24,8 @@
         
         $('#department-table').DataTable({
             
-            "order": [[ 1, "desc" ]], 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
             "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
@@ -33,58 +33,66 @@
         });
      
         $('#function-table').DataTable({
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
             
         });
         $('#team-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
         });
         $('#subteam-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
         });
         $('#designation-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
         });
         $('#region-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
         });
         $('#state-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
         });
         $('#city-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
         });
         $('#location-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
@@ -119,8 +127,9 @@
             scrollX: true,
         });
         $('#wages-table').DataTable({
-            "order": [[ 1, "desc" ]], //or asc 
-            "columnDefs" : [{"targets":1, "type":"date-eu"}],
+            "order": [[ 1, "desc" ],[ 2, "desc" ]], 
+            "columnDefs" : [{"targets":1, "type":"date-eu"},{"targets":2, "type":"date-eu"}],
+            "pageLength": 10,
             "pagingType": "full_numbers",
             "paging": true,
             "lengthMenu": [10, 25, 50, 75, 100],
