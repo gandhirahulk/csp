a6284c65298ca5147c4e6debf3847550f96d18a3
Sadaf Shaikh <65351945+sadaf-shaikh@users.noreply.github.com>
2020-12-03 16:14:54 +0530

    project_name in installed_apps

--- Candidates_Selection_project/settings.py
+++ Candidates_Selection_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'csp_app',
+    'Candidates_Selection_project',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
