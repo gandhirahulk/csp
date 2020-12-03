652d4405ac50126ea7a44b1e861597bc0bde204d
Sadaf Shaikh <65351945+sadaf-shaikh@users.noreply.github.com>
2020-12-03 17:09:11 +0530

    #2

--- Candidates_Selection_project/urls.py
+++ Candidates_Selection_project/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path, include
 from csp_app import views
+from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf import settings
 
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', include(('csp_app.urls', 'csp_app'), namespace='csp_app')),
     path('', include('django.contrib.auth.urls')),
     
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
